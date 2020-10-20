3d361177274c43c481bad853863937c1e7de6e84
Alanna Bouloy <lanihope@gmail.com>
2020-10-20 11:52:37 -0400

    added playstore-data

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const playstore = require('./playstore-data');
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
 
 app.get('/apps', (req, res) => {
+    res
+        .json(playstore);
 
 });
 
